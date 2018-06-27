b019dd6e97166c8665f3fd5a77b5b349b7f0aa05
houfang <houfangcq@gmail.com>
2018-06-27 08:00:37 +0800

    add jersey

---
+++ jersey-example/src/main/java/com/example/demo/JerseyApplication.java
@@ -0,0 +1,13 @@
+
+package com.example.demo;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class JerseyApplication extends ResourceConfig {
+    public JerseyApplication() {
+       register(JacksonFeature.class);
+    }
+}
+
+

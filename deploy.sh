5584de966ecd0021a6c4e594ca56285af02c8381
houfang <houfangcq@gmail.com>
2018-06-30 15:41:26 +0800

    learn tic tac tutorial

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 cd my-app
 npm run build
-sudo cp -r build /Library/tomcat/webapps/
+sudo cp -r build/* /Library/tomcat/webapps/react
 cd -
 cd jersey-example
 gradle build
 sudo cp build/libs/jersey-example.war /Library/tomcat/webapps
 cd -
+sudo sh /Library/tomcat/bin/startup.sh
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Game from './tutorial/tutorial';
+import './tutorial/tutorial.css';
 
 ReactDOM.render(
-  <App />,
+  <Game />,
   document.getElementById('root')
 );

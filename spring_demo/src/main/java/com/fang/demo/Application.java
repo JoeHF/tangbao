8737fa14a1655f32ca4577f6528f9446aa188847
houfang <houfangcq@gmail.com>
2018-07-01 16:07:20 +0800

    add spring restful demo

---
+++ spring_demo/src/main/java/com/fang/demo/Application.java
@@ -0,0 +1,12 @@
+package com.fang.demo;
+
+import org.joda.time.LocalTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
